211cef4d7e1fcc34ddcddd29fe1fc95008dea2d7
johnnybeatz <92400293+johnnybeatz@users.noreply.github.com>
2024-02-09 22:59:29 +0300

    ability to add a message

--- Assistant/database.py
+++ Assistant/database.py
@@ -9,11 +9,16 @@ client = MongoClient(os.environ.get("MongoConnection"))
 db = client['AirbnbAssistant']
 Users = db['Users']  
 
+instruction = {"role": "system","content": "you are help full assistant. you assist our customers by answering questions about our property we have on airbnb. you only assist users with only our property and business realted question. call wrong_prompt when user prompt is not related to our service and business."}
 
-def reset_conversation(id_):
-    Users.update_one({"_id":int(id_)},{"$set":{"conversation":[{"role": "system","content": "you are help full assistant. you assist our customers by answering questions about our property we have on airbnb. you only assist users with only our property and business realted question. call wrong_prompt when user prompt is not related to our service and business."}]}})
+def reset_conversation(_id):
+    Users.update_one({"_id":int(_id)},{"$set":{"conversation":[instruction]}})
 
-def register(id_,first_name,username):
-    existance = Users.find_one({"_id":int(id_)})
+def register(_id,first_name,username):
+    existance = Users.find_one({"_id":int(_id)})
     if existance == None:
-        Users.insert_one({"_id":id_,"firstName":first_name,"userName":username,"email":"","personalName":"","conversation":[]})
+        Users.insert_one({"_id":_id,"firstName":first_name,"userName":username,"email":"","personalName":"","conversation":[instruction]})
+
+def add_message(_id,message):
+    conversation = list(Users.find_one({"_id":_id})["conversation"])
+    Users.update_one({"_id":int(_id)},{"$set":{"conversation":conversation.append(message)}})
