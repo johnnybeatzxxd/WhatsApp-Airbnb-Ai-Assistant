b6c844c0543c4dab78c2f5c1c6368ca30141fa22
johnnybeatz <92400293+johnnybeatz@users.noreply.github.com>
2024-02-09 18:48:01 +0300

    POC

--- Assistant/views.py
+++ Assistant/views.py
@@ -8,15 +8,27 @@ import telebot
 import datetime
 import os
 import time
+from dotenv import load_dotenv
+
+load_dotenv()
+
+bot = telebot.TeleBot(os.environ.get("TelegramBotToken"))
+
 
 # Create your views here.
 
 class TelegramWebhookView(View):
-    
+    print(os.environ.get("TelegramBotToken"))
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
 
-    print('hellow world')
     
-    
+    @bot.add_message_handler(func=lambda x:True)
+    def start(message):
+        bot.reply_to(message,"👀 Sorry friend! Didn't understand that one.")
+
+
+    def post(self, request):        
+        bot.process_new_updates([telebot.types.Update.de_json(request.body.decode("utf-8"))])
+        return HttpResponse("!", status=200)
