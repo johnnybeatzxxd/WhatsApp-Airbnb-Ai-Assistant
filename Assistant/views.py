25e207d0e9b5733d91550a0452bbf6eae413235c
johnnybeatz <92400293+johnnybeatz@users.noreply.github.com>
2024-02-09 19:07:41 +0300

    reset buttom added to the bot

--- Assistant/views.py
+++ Assistant/views.py
@@ -5,6 +5,7 @@ from django.utils.decorators import method_decorator
 from django.http import HttpResponse
 #from pymongo import MongoClient
 import telebot
+import openai
 import datetime
 import os
 import time
@@ -13,12 +14,17 @@ from dotenv import load_dotenv
 load_dotenv()
 
 bot = telebot.TeleBot(os.environ.get("TelegramBotToken"))
-
+openai.api_key = os.environ.get('OpenAiKey')
 
 # Create your views here.
+def markups():
+    markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True,row_width=2)   
+    reset = telebot.types.KeyboardButton('💁‍♂ Reset')   
+    markup.add(reset)
+    return markup
 
 class TelegramWebhookView(View):
-    print(os.environ.get("TelegramBotToken"))
+    
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
@@ -26,7 +32,7 @@ class TelegramWebhookView(View):
     
     @bot.message_handler(func=lambda x:True)
     def start(message):
-        bot.reply_to(message,"👀 Sorry friend! Didn't understand that one.")
+        bot.reply_to(message,"👀 Sorry friend! Didn't understand that one.",reply_markup=markups())
 
 
     def post(self, request):        
