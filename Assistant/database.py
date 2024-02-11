08ba7ca5dc3358221c676a74c3360d22f35c4366
johnnybeatz <92400293+johnnybeatz@users.noreply.github.com>
2024-02-11 21:30:19 +0300

    the ability to check missing info and set infos added

--- Assistant/database.py
+++ Assistant/database.py
@@ -14,7 +14,7 @@ instruction = {"role": "system","content": "you are help full assistant. you ass
 def reset_conversation(_id):
     Users.update_one({"_id":int(_id)},{"$set":{"conversation":[instruction]}})
 
-def register(_id,first_name,username):
+def register(_id,first_name,username): 
     existance = Users.find_one({"_id":int(_id)})
     if existance == None:
         Users.insert_one({"_id":_id,"firstName":first_name,"userName":username,"email":"","personalName":"","conversation":[instruction]})
@@ -22,3 +22,20 @@ def register(_id,first_name,username):
 def add_message(_id,message):
     conversation = list(Users.find_one({"_id":_id})["conversation"])
     Users.update_one({"_id":int(_id)},{"$set":{"conversation":conversation.append(message)}})
+
+def required_user_info(_id): # returns user Email and Name
+    required_info = {}
+    email = Users.find_one({"_id":_id}).get("email")
+    personalName = Users.find_one({"_id":_id}).get("personalName")
+    user_info = {"email":email,"personalName":personalName}
+    required_info = []
+    if user_info["email"] == "":
+        required_info.append("email")
+    if user_info["personalName"] == "":
+        required_info.append("personalName")
+    return required_info
+
+def set_user_info(_id,info):
+    Users.update_one({"_id":int(_id)},{"$set":info})
+
+    
