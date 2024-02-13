1e771702d5576c0e96ba057d0825ceaf9ab95180
johnnybeatz <92400293+johnnybeatz@users.noreply.github.com>
2024-02-13 07:11:41 +0300

    making it to log every step to make deubing easy

--- Assistant/ai.py
+++ Assistant/ai.py
@@ -214,16 +214,14 @@ def function_call(response,_id):
 def generate_response(_id,messages,required_user_info,):
     print("generating answer ... ")
     print(messages)
-    try:
-        response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo-1106",
-        messages=list(messages),
-        functions=function_descriptions,
-        function_call="auto",
-        temperature = 0.1)
-    except:
-        print('error')
-    print(response["choices"][0]["message"])
+    
+    response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo-1106",
+    messages=list(messages),
+    functions=function_descriptions,
+    function_call="auto",
+    temperature = 0.1)
+
     print(response)
     while response["choices"][0]["finish_reason"] == "function_call":
         
@@ -257,6 +255,8 @@ def generate_response(_id,messages,required_user_info,):
                 
             #print(response["choices"][0]["message"])
     return response["choices"][0]["message"]["content"]
+
+#testing
 message = [{"role": "system", "content": "You are a helpful assistant."},
     {"role": "user", "content": "Who won the world series in 2020?"},]
 generate_response(123,message,{})
