56d2e40ab12a0efe3960953ef0e462ce45f5b279
johnnybeatzxxd <johnnybeatzxxd@gmail.com>
2024-04-21 19:08:17 +0300

    fixed the reset function

--- Assistant/views.py
+++ Assistant/views.py
@@ -67,7 +67,7 @@ class TelegramWebhookView(View):
         username = customer.from_user.username
         id_ = customer.chat.id
         
-        if prompt == '💁‍♂ Reset':
+        if prompt[0]["text"] == '💁‍♂ Reset':
             database.reset_conversation(id_)
 
         else:
