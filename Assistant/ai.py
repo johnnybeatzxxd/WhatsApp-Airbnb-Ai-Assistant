17ea37080496a68560a542f19a66e1c7ca7e2614
johnnybeatzxxd <johnnybeatzxxd@gmail.com>
2024-04-22 21:46:53 +0300

    added safety settings

--- Assistant/ai.py
+++ Assistant/ai.py
@@ -251,6 +251,24 @@ class llm:
                 "tools": [{
                     "functionDeclarations": self.function_descriptions
                     }],
+                "safetySettings": [
+            {
+                "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+                "threshold": "BLOCK_ONLY_HIGH"
+            },
+            {
+                "category": "HARM_CATEGORY_HARASSMENT",
+                "threshold": "BLOCK_ONLY_HIGH"
+            },
+            {
+                "category": "HARM_CATEGORY_HATE_SPEECH",
+                "threshold": "BLOCK_ONLY_HIGH"
+            },
+            {
+                "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+                "threshold": "BLOCK_ONLY_HIGH"
+            },
+        ],
                 "generationConfig": {
                 "temperature": 0.1,
                 "topK": 1,
