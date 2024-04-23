d2ba192b9cbc9d753c83ff2e6eb660e71f67e9f9
johnnybeatzxxd <johnnybeatzxxd@gmail.com>
2024-04-23 22:14:57 +0300

    test 1.6

--- Assistant/ai.py
+++ Assistant/ai.py
@@ -363,7 +363,7 @@ class llm:
                             print("Empty JSON response received, retrying...")
                             functionResponse = functionResponse.append({"text": "please generate a response"})
                             messages[-1]["parts"] = functionResponse
-                            print(data)
+                            print(messages[-1])
                     else:
                         print(f"Received non-200 status code: {response.status_code}")
                     
